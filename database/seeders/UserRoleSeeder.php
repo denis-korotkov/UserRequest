c4a01bec1aaf43a7e6a3881bb465d2f4eae69a5b
denis-korotkov <deniskorotkov99@gmail.com>
2024-08-25 18:09:09 +0700

    Сиды ролей и тестовый пользователь

---
+++ database/seeders/UserRoleSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+
+use App\Models\UserRole;
+use Illuminate\Database\Seeder;
+
+class UserRoleSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        UserRole::factory()->create([
+            ['role' => 'admin'],
+            ['role' => 'user'],
+        ]);
+    }
+}
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'test',
+            'email' => 'test@example.com',
+            'user-role' => 'test@example.com',
+            'password' => '123',
+        ]);
+    }
+}
