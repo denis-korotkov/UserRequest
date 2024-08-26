5da142e82700d4d691c55123283465fe5843a4a4
denis-korotkov <deniskorotkov99@gmail.com>
2024-08-26 23:21:21 +0700

    makefile.sh fix

--- makefile.sh
+++ makefile.sh
@@ -2,11 +2,13 @@
 
 cp .env.example .env
 
-echo "Start composer install for [laravel.test]..."
-docker-compose exec laravel.test composer install
+echo "Start composer install"
+composer install
+
+vendor/bin/sail up -d
 
 echo "Start migrate for [laravel.test]..."
 docker-compose exec laravel.test php artisan migrate
 
 echo "Start seed for [laravel.test]..."
-docker-compose exec laravel.test db:seed
+docker-compose exec laravel.test php artisan db:seed
