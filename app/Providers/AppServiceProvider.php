30242515a97f160fa2190ff8eeb736e7738cc9ef
denis-korotkov <deniskorotkov99@gmail.com>
2024-08-26 17:48:02 +0700

    Вынес управление репозиторием UserRole в конфиги

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repositories\UserRoleCachedRepository;
+use App\Repositories\UserRoleDatabaseRepository;
+use App\Repositories\UserRoleRepositoryInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,20 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(UserRoleCachedRepository::class, function(){
+            return new UserRoleCachedRepository(
+                config('app_service.cached_repository_cache_time'),
+                config('app_service.cached_repository_cache_prefix'),
+                new UserRoleDatabaseRepository()
+            );
+        });
+
+        $this->app->bind(UserRoleRepositoryInterface::class, function(){
+            $userRoleRepositoryConfig = config('app_service.user_role_repository');
+            $class = 'App\Repositories\UserRole' . ucfirst($userRoleRepositoryConfig) . 'Repository';
+
+            return $this->app->make($class);
+        });
     }
 
     /**
